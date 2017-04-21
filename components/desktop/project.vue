d6296298089d77d0dd3a5d158a7ac5fe896db24b
Lurker <434848237@qq.com>
2017-04-21 12:48:29 +0800

    add 项目

--- components/desktop/project.vue
+++ components/desktop/project.vue
@@ -1,13 +1,19 @@
 <template>
-  <div>
-    11
+  <div class="projectBox">
+    <projectItem></projectItem>
   </div>
 </template>
 
 <style lang="less">
-  
+  .projectBox{position: absolute; top: 0;left: 0; z-index: 2}
 </style>
 
 <script>
+  import projectItem from '../project/item'
   
+  export default {
+    components: {
+      projectItem
+    }
+  }
 </script>
---
+++ components/project/item.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="projectItem" v-if="items">
+    <img :src="items.icon" class="projectItemIcon"/>
+    <p class="projectItemText">{{ items.text }}</p>
+  </div>
+</template>
+
+<style lang="less">
+  .projectItem{width: 60px; padding: 10px 20px; text-align: center;
+    &:hover{background-color: rgba(255, 255, 255, .5)}
+    .projectItemIcon{width: 100%}
+    .projectItemText{color: #fff;}
+  }
+</style>
+
+<script>
+  export default {
+    props: {
+      items: {
+        type: Object,
+        default: () => {}
+      }
+    }
+  }
+</script>
